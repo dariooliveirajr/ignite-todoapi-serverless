0444e0674f12ec59c2a2cb04ee827405ccb19bc5
dariooliveirajr <dario@duotalk.io>
2021-05-16 12:16:19 -0300

    Create basic configuration and route access

---
+++ src/functions/hello.ts
@@ -0,0 +1,14 @@
+
+
+
+export const handle = async (event) => {
+    return {
+        statusCode: 201,
+        body: JSON.stringify({
+            message: "Hello World Ignite Serverless",
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }
+}
